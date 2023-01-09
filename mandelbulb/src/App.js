b09b7e93d08a012cedf8ee50af78f19cad4417a7
Kolton Koester <koltonkoester10@gmail.com>
2023-01-09 10:13:07 -0500

    mandelBulb is formed without confined limitations of points

--- mandelbulb/src/App.js
+++ mandelbulb/src/App.js
@@ -12,8 +12,7 @@ function App() {
   const DIM = 32;
   const  mandelBulb = new Array();
   const n = 8;
-  
-  let maxIterations = 20;
+  let maxIterations = 30;
   let iteration = 0;
 
   function map(value, start1, stop1, start2, stop2) {
@@ -23,6 +22,7 @@ function App() {
   const point = ()=>{
     for (let i = 0; i < DIM; i++) {
       for (let j = 0; j < DIM; j++) {
+        let edge = false;
         for (let k = 0; k < DIM; k++) {
 
           let x = map(i, 0, DIM, -1, 1);
@@ -31,15 +31,15 @@ function App() {
           
           let zeta = new Vector3(0,0,0);
           while (true) {
-            let r = Math.sqrt(Math.pow(zeta.x,2) + Math.pow(zeta.y,2) + Math.pow(zeta.z,2));
-            let theta = Math.atan2( Math.sqrt(Math.pow(zeta.x,2) + Math.pow(zeta.y,2)), zeta.z);
-            let phi = Math.atan2( zeta.y, zeta.x);
+            let r = Math.sqrt(Math.pow(x,2) + Math.pow(y,2) + Math.pow(z,2));
+            let theta = Math.atan2( Math.sqrt(Math.pow(x,2) + Math.pow(y,2)), z);
+            let phi = Math.atan2( y, x);
 
             let cZ = new Vector3(r,theta,phi);
 
-            let newx = Math.pow(cZ.x, n) * Math.sin(cZ.y*n) * Math.cos(cZ.z*n);
-            let newy = Math.pow(cZ.x, n) * Math.sin(cZ.y*n) * Math.sin(cZ.z*n);
-            let newz = Math.pow(cZ.x, n) * Math.cos(cZ.y*n);
+            let newx = Math.pow(r, n) * Math.sin(theta * n) * Math.cos(phi * n);
+            let newy = Math.pow(r, n) * Math.sin(theta * n) * Math.sin(phi * n);
+            let newz = Math.pow(r, n) * Math.cos(theta * n);
 
             zeta.x = newx + x;
             zeta.y = newy + y;
@@ -47,13 +47,18 @@ function App() {
             
             iteration++
             
-            if (cZ.x > 16) {
+            if (r > 16) {
+              if (edge) {
+                edge = false;
+              }
               console.log("you're in the if cZ if > 16");
               break;
             }
-            
             if (iteration > maxIterations) {
-              mandelBulb.push(zeta.x,zeta.y,zeta.z);
+              
+                mandelBulb.push(zeta.x,zeta.y,zeta.z);
+                edge = true;
+              
               break;
             } 
           }          
@@ -65,8 +70,7 @@ function App() {
     const points = useMemo (() => {
       console.log(mandelBulb);
       point();
-      return new Float32BufferAttribute(
-       mandelBulb,3)
+      return new Float32BufferAttribute(mandelBulb,3)
     }, []);
   
     return (
@@ -75,7 +79,7 @@ function App() {
           <bufferAttribute attach={"attributes-position"} {...points} />
         </bufferGeometry>
         <pointsMaterial
-          size={.001}
+          size={.0001}
           threshold={0.5}
           color={0xffff}
           sizeAttenuation={true}
